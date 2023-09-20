9cc0a9cb754d67e0aac3f2f070b8bd367610c318
Nightvision8D <127532296+Nightvision8D@users.noreply.github.com>
2023-09-20 10:09:47 +0200

    Logo
    Added Logo and standard-navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./menu/menu.component').then((m) => m.MenuComponent),
     children: [
       {
         path: '',
         pathMatch: 'full',
-        loadComponent: () =>
-          import('./menu/menu.component').then((m) => m.MenuComponent),
+        loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
       },
     ],
   },
