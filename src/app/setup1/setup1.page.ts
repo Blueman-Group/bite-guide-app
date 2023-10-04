bfbe9d4e677062ce72966e2fdfdf9b7bcbdf9206
dmncrs <domenic.roos@icloud.com>
2023-10-04 20:59:20 +0200

    Add next button

--- src/app/setup1/setup1.page.ts
+++ src/app/setup1/setup1.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { StorageService } from '../services/storage.service';
 import { Canteen } from '../interfaces/canteen';
 
@@ -11,13 +11,16 @@ import { Canteen } from '../interfaces/canteen';
   templateUrl: './setup1.page.html',
   styleUrls: ['./setup1.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
 export class Setup1Page implements OnInit, AfterContentChecked {
   constructor(private router: Router, public storageService: StorageService) {}
 
   public canteens: Canteen[] = [];
   updating = false;
+  document = document;
+
+  canteen: string = '';
 
   customPopoverOptions = {
     cssClass: 'setup-popover',
