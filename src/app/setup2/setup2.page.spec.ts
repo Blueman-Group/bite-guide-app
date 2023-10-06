1512985db24166c942c6ebdf89643a56b3106fc0
dmncrs <domenic.roos@icloud.com>
2023-10-06 20:23:55 +0200

    Fix test files

--- src/app/setup2/setup2.page.spec.ts
+++ src/app/setup2/setup2.page.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { Setup2Page } from './setup2.page';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 describe('Setup2Page', () => {
   let component: Setup2Page;
   let fixture: ComponentFixture<Setup2Page>;
 
   beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicStorageModule.forRoot()],
+      providers: [Storage],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(Setup2Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
