c36617e99c5a07aac102c3dab4e460c9934b7286
dmncrs <domenic.roos@icloud.com>
2023-10-06 15:55:41 +0200

    Change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,26 +6,32 @@ export const routes: Routes = [
     pathMatch: 'full',
     loadComponent: () => import('./start/start.page').then((m) => m.StartPage),
   },
+
   {
-    path: '',
+    path: 'home',
     loadComponent: () =>
       import('./menu/menu.component').then((m) => m.MenuComponent),
     children: [
       {
-        path: 'home',
+        path: '',
         pathMatch: 'full',
         loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
       },
     ],
   },
   {
-    path: 'setup-1',
-    loadComponent: () =>
-      import('./setup1/setup1.page').then((m) => m.Setup1Page),
-  },
-  {
-    path: 'setup-2',
-    loadComponent: () =>
-      import('./setup2/setup2.page').then((m) => m.Setup2Page),
+    path: 'setup',
+    children: [
+      {
+        path: '1',
+        loadComponent: () =>
+          import('./setup1/setup1.page').then((m) => m.Setup1Page),
+      },
+      {
+        path: '2',
+        loadComponent: () =>
+          import('./setup2/setup2.page').then((m) => m.Setup2Page),
+      },
+    ],
   },
 ];
