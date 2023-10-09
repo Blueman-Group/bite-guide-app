a48280bda05c8bbda81637908ca01c136790220e
dmncrs <domenic.roos@icloud.com>
2023-10-09 14:04:51 +0200

    Add canteen selection

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { StorageCanteen } from '../interfaces/storage-canteen';
+import { StorageService } from '../services/storage.service';
+import { Canteen } from '../interfaces/canteen';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class HomePage implements OnInit {
-  constructor(private router: Router) {}
+export class HomePage implements OnInit, AfterContentChecked {
+  selectedCantine: string = '';
+  selectedCantineData: StorageCanteen | null = null;
+  canteens: Canteen[] = [];
+  updating = false;
+
+  constructor(private router: Router, private storageService: StorageService) {}
 
   ngOnInit(): void {
     if (!this.router.navigated) this.router.navigate(['/']);
+  }
+
+  async ngAfterContentChecked() {
+    if (!this.updating) {
+      this.updating = true;
+      this.selectedCantineData = await this.storageService.getFavoriteCanteen();
+      this.selectedCantine = this.selectedCantineData.canteen._key;
+      this.canteens = await this.storageService.getCanteens();
+    }
+  }
+
+  async onSelectChange() {
+    await this.storageService.updateMenus(this.selectedCantine);
+    let storageCanteen = await this.storageService.getCanteen(
+      this.selectedCantine
+    );
+    this.selectedCantineData = storageCanteen;
+    console.log(this.selectedCantineData);
   }
 }
