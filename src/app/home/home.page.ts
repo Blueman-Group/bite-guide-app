aca98248243eb6deddc4f454aae47baf89527e14
dmncrs <domenic.roos@icloud.com>
2023-10-09 13:38:44 +0200

    Change route

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { DatabaseService } from '../services/database.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +9,10 @@ import { DatabaseService } from '../services/database.service';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
-  constructor(private databaseService: DatabaseService) {
-    this.databaseService
-      .getMealsAtDate(new Date(), {
-        _key: '2',
-        name: 'Mensa Academica',
-        location: 'Reichenhainer Str. 55, 09126 Chemnitz',
-      })
-      .then((meals) => {
-        console.log(meals);
-      });
+export class HomePage implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (!this.router.navigated) this.router.navigate(['/']);
   }
 }
