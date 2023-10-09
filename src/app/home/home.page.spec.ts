77142cacc20b8ce58ba3aee7e4084920a1f131f7
dmncrs <domenic.roos@icloud.com>
2023-10-09 14:10:17 +0200

    fix homepage test

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,12 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { RouterModule } from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [IonicStorageModule.forRoot(), RouterModule.forRoot([])],
+      providers: [Storage],
+    }).compileComponents();
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
