991078b2d7cd7eabf27d86128db0747dc6877fea
Nightvision8D <127532296+Nightvision8D@users.noreply.github.com>
2023-10-10 20:59:20 +0200

    Settingpage and changed from component to page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,9 +13,26 @@ export const routes: Routes = [
       import('./menu/menu.component').then((m) => m.MenuComponent),
     children: [
       {
-        path: '',
+        path: 'main',
         pathMatch: 'full',
         loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./settings/settings.page').then((m) => m.SettingsPage),
+      },
+      {
+        path: 'history',
+        loadComponent: () =>
+          import('./history/history.page').then((m) => m.HistoryPage),
+      },
+      {
+        path: 'own-mealplan',
+        loadComponent: () =>
+          import('./own-mealplan/own-mealplan.page').then(
+            (m) => m.OwnMealplanPage
+          ),
       },
     ],
   },
