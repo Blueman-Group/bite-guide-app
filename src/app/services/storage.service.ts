803881b000e8923063bcb36dc974f62213634c5b
dmncrs <domenic.roos@icloud.com>
2023-10-12 11:18:56 +0200

    Refactor and commenting

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -12,10 +12,10 @@ export class StorageService {
   private _storage: Storage | null = null;
   _storageReady = false;
 
-  constructor(private storage: Storage, private databaseService: DatabaseService) {
-    this.init().then(() => console.log('storage service ready'));
-  }
+  //inject Storage and DatabaseService
+  constructor(private storage: Storage, private databaseService: DatabaseService) {}
 
+  /**init storage on device with create methode and check if it is ready*/
   async init() {
     this._storage = await this.storage.create();
     await this._storage.set('readyTest', 'ready');
@@ -47,6 +47,9 @@ export class StorageService {
     await this._storage?.set(key, canteen);
   }
 
+  /**Get a list of all canteens saved in storage
+   * @returns List of all canteens saved in storage
+   */
   async getCanteens(): Promise<Canteen[]> {
     let canteens: Canteen[] = [];
     const keys = await this._storage?.keys();
@@ -60,10 +63,18 @@ export class StorageService {
     return canteens;
   }
 
+  /**Add a canteen to storage
+   * @param key Key of storage
+   * @param canteen Canteen Object to save
+   */
   async addCanteen(key: string, canteen: Canteen): Promise<void> {
     await this._storage?.set(key, { canteen, menu: [] });
   }
 
+  /**Add the menu of a canteen to storage
+   * @param canteen Canteen Object in which the menu should be saved
+   * @param menu Menu Object to save
+   */
   async addMenu(canteen: Canteen, menu: { date: string; meals: Meal[] }): Promise<void> {
     if (!(await this.checkCanteen(canteen._key))) {
       await this.addCanteen(canteen._key, canteen);
@@ -74,6 +85,11 @@ export class StorageService {
     await this._storage?.set(canteen._key, _storageCanteen);
   }
 
+  /**Get the menu of a canteen at a specific date
+   * @param canteen Canteen Object of which the menu should be returned
+   * @param date Date of which the menu should be returned
+   * @returns Menu of the canteen at the specific date
+   */
   async getMenu(canteen: Canteen, date: Date): Promise<Meal[]> {
     const storageCanteen = await this.getCanteen(canteen._key);
     let menu = storageCanteen.menu.find((m: { date: string; meals: Meal[] }) => m.date === date.toISOString().substring(0, 10));
@@ -89,42 +105,63 @@ export class StorageService {
     return databaseMeals;
   }
 
+  /**Update the menus of a specific canteen on all dates in the next 2 weeks
+   * @param key Key of the canteen which should be updated
+   */
   public async updateMenus(key: string): Promise<void> {
     let storageCanteen = await this.getCanteen(key);
     let date = new Date();
-    storageCanteen.menu = storageCanteen.menu.filter((m: { date: string; meals: Meal[] }) => new Date(m.date).getWeek() <= date.getWeek());
+    storageCanteen.menu = storageCanteen.menu.filter((m: { date: string; meals: Meal[] }) => getWeek(new Date(m.date)) <= getWeek(date));
 
     let itDate = new Date();
     if (storageCanteen.menu.length < 10) {
-      if (storageCanteen.menu.find((m: { date: string; meals: Meal[] }) => new Date(m.date).getWeek() === date.getWeek())) {
-        itDate.setToNextWeek();
-        for (let i = 0; i < 5; i++) {
-          await this.getMenu(storageCanteen.canteen, itDate);
-          itDate.setDate(itDate.getDate() + 1);
-        }
+      if (storageCanteen.menu.find((m: { date: string; meals: Meal[] }) => getWeek(new Date(m.date)) === getWeek(date))) {
+        setToNextWeek(itDate);
+        this._updateWeek(itDate, storageCanteen);
       } else {
-        itDate.setToCurrentWeek();
-        for (let i = 0; i < 5; i++) {
-          await this.getMenu(storageCanteen.canteen, itDate);
-          itDate.setDate(itDate.getDate() + 1);
-        }
-        itDate.setToNextWeek();
-        for (let i = 0; i < 5; i++) {
-          await this.getMenu(storageCanteen.canteen, itDate);
-          itDate.setDate(itDate.getDate() + 1);
-        }
+        setToCurrentWeek(itDate);
+        this._updateWeek(itDate, storageCanteen);
+        setToNextWeek(itDate);
+        this._updateWeek(itDate, storageCanteen);
       }
+    } else {
+      storageCanteen.menu
+        .filter((m: { date: string; meals: Meal[] }) => getWeek(new Date(m.date)) > getWeek(date))
+        .forEach((m: { date: string; meals: Meal[] }) => {
+          storageCanteen.menu.splice(storageCanteen.menu.indexOf(m), 1);
+        });
+      setToNextWeek(itDate);
+      this._updateWeek(itDate, storageCanteen);
     }
   }
 
+  /**
+   * Update the menu of a canteen on a specific week
+   * @param weekDate Date of the week which should be updated
+   * @param storageCanteen StorageCanteen Object of which the menu should be updated
+   */
+  private async _updateWeek(weekDate: Date, storageCanteen: StorageCanteen) {
+    for (let i = 0; i < 5; i++) {
+      await this.getMenu(storageCanteen.canteen, weekDate);
+      weekDate.setDate(weekDate.getDate() + 1);
+    }
+  }
+
+  /**
+   * Get the meal object of a canteen which are actual. Actual means that the date of the meal is not in the past
+   * @param key Key of the canteen
+   * @returns Array of meal objects which are actual
+   */
   public async getActualMeals(key: string): Promise<{ date: string; meals: Meal[] }[]> {
     let storageCanteen = await this.getCanteen(key);
     let date = new Date();
-    let menu = storageCanteen.menu.filter((m: { date: string; meals: Meal[] }) => new Date(m.date).getWeek() <= date.getWeek());
+    let menu = storageCanteen.menu.filter((m: { date: string; meals: Meal[] }) => getWeek(new Date(m.date)) <= getWeek(date));
     return menu;
   }
 
-  //a method names updateCanteens which updates the existing canteens in the local storage with the actual ones from the datbase
+  /**
+   * Updates the existing canteens in the local storage with the actual ones from the datbase
+   */
   public async updateCanteens(): Promise<void> {
     let canteens = await this.databaseService.getCanteens();
     for (let canteen of canteens) {
@@ -134,72 +171,83 @@ export class StorageService {
     }
   }
 
+  /**
+   * Set the favorite canteen
+   * @param key Key of the canteen
+   */
   public async setFavorite(key: string): Promise<void> {
     await this._storage?.set('favorite', key);
   }
 
+  /**
+   * Get the favorite canteen
+   * @returns Favorite canteen as StorageCanteen object
+   */
   public async getFavoriteCanteen(): Promise<StorageCanteen> {
     return await this.getCanteen(await this._storage?.get('favorite'));
   }
 
+  /**
+   * Set favorite colormode of user
+   * @param colorMode Color mode which is preferred by the user (dark or light)
+   */
   async setColorMode(colorMode: 'dark' | 'light'): Promise<void> {
     await this._storage?.set('colormode', colorMode);
   }
 
+  /**
+   * Get the favorite colormode selected by the user
+   * @returns Favorite colormode of user (dark or light)
+   */
   async getColorMode(): Promise<'dark' | 'light'> {
     return await this.storage?.get('colormode');
   }
 }
 
-declare global {
-  interface Date {
-    getWeek(): number;
-    setToNextWeek(): void;
-    setToCurrentWeek(): void;
-  }
+/**
+ * A function which sets the date to the beginning of the current
+ * @param date Date which should be set to the beginning of the current week
+ */
+function setToCurrentWeek(date: Date) {
+  let day = date.getDay();
+  let diff = day - 1;
+  date.setDate(day - diff);
 }
 
-Date.prototype.setToCurrentWeek = function () {
-  //a function which sets the date to the beginning of the current week
-  let day = this.getDay();
-  let diff = day - 1;
-  this.setDate(this.getDate() - diff);
-};
-
-Date.prototype.setToNextWeek = function () {
-  //a function which sets the date to the beginning of next week
-  let day = this.getDay();
+/**
+ * A function which sets the date to the beginning of next week
+ * @param date Date which should be set to the beginning of the next week
+ */
+function setToNextWeek(date: Date) {
+  let day = date.getDay();
   let diff = 8 - day;
-  this.setDate(this.getDate() + diff);
-};
+  date.setDate(day + diff);
+}
 
-Date.prototype.getWeek = function () {
+/**
+ * A function which returns the week number of the date
+ * @param date Date of which the week number should be returned
+ */
+function getWeek(date: Date): number {
   let dowOffset = 1; //start week on monday
-  let newYear = new Date(this.getFullYear(), 0, 1);
+  let newYear = new Date(date.getFullYear(), 0, 1);
   let day = newYear.getDay() - dowOffset; //the day of week the year begins on
   day = day >= 0 ? day : day + 7;
   let x;
-  let daynum =
-    Math.floor(
-      (this.getTime() -
-        newYear.getTime() -
-        (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
-        86400000
-    ) + 1;
+  let daynum = Math.floor((date.getTime() - newYear.getTime() - (date.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) / 86400000) + 1;
   let weeknum;
   //if the year starts before the middle of a week
   if (day < 4) {
     weeknum = Math.floor((daynum + day - 1) / 7) + 1;
     if (weeknum > 52) {
-      let nYear = new Date(this.getFullYear() + 1, 0, 1);
+      let nYear = new Date(date.getFullYear() + 1, 0, 1);
       let nday = nYear.getDay() - dowOffset;
       nday = nday >= 0 ? nday : nday + 7;
-      /*if the next year starts before the middle of
-              the week, it is week #1 of that year*/
+      //if the next year starts before the middle of the week, it is week #1 of that year
       weeknum = nday < 4 ? 1 : 53;
     }
   } else {
     weeknum = Math.floor((daynum + day - 1) / 7);
   }
   return weeknum;
-};
+}
