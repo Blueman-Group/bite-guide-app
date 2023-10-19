8af21a81f6dd0e56798d0366e4cf1508d36c5616
dmncrs <domenic.roos@icloud.com>
2023-10-19 11:35:44 +0200

    Add angular

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /// <reference types="@angular/localize" />
 
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -13,6 +13,7 @@ import { Drivers } from '@ionic/storage';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -24,12 +25,18 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
-    importProvidersFrom(
-      IonicStorageModule.forRoot({
+    importProvidersFrom(IonicStorageModule.forRoot({
         name: '__biteGuideStorage',
         driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
-      })
-    ),
+    })),
     provideRouter(routes),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
