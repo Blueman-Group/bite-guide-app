2a298d7390b604fa212fb4ad93f8789c066831b0
Nightvision8D <127532296+Nightvision8D@users.noreply.github.com>
2023-10-22 14:59:24 +0200

    Swiper für den eigenen Mealplan hinzugefügt

--- src/app/own-mealplan/own-mealplan.page.ts
+++ src/app/own-mealplan/own-mealplan.page.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicSlides } from '@ionic/angular';
+import { register } from 'swiper/element/bundle';
+import Swiper from 'swiper';
+
+import { NavbarHeaderComponent } from '../navbar-header/navbar-header.component';
+import swiper from 'swiper';
 
 @Component({
   selector: 'app-own-mealplan',
   templateUrl: './own-mealplan.page.html',
   styleUrls: ['./own-mealplan.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [IonicModule, CommonModule, FormsModule, NavbarHeaderComponent],
 })
-export class OwnMealplanPage {}
+export class OwnMealplanPage {
+  swiperModules = [IonicSlides];
+  constructor() {
+    register();
+  }
+  test(swiper: Swiper) {
+    console.log(swiper.swipeDirection);
+    if (swiper.swipeDirection === 'next') {
+      console.log('next');
+    } else {
+      console.log('prev');
+    }
+  }
+}
