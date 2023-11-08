bd2fc90e3e2912421dd7f257cf12b35f31b2ecc6
dmncrs <domenic.roos@icloud.com>
2023-11-08 12:09:29 +0100

    Add wait for start

--- src/app/own-mealplan/own-mealplan.page.ts
+++ src/app/own-mealplan/own-mealplan.page.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterContentChecked, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicSlides } from '@ionic/angular';
@@ -7,6 +7,8 @@ import Swiper from 'swiper';
 import { StorageService } from '../services/storage.service';
 
 import { NavbarHeaderComponent } from '../navbar-header/navbar-header.component';
+import { EventAggregatorService } from '../services/event-aggregator.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-own-mealplan',
@@ -16,16 +18,34 @@ import { NavbarHeaderComponent } from '../navbar-header/navbar-header.component'
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [IonicModule, CommonModule, FormsModule, NavbarHeaderComponent],
 })
-export class OwnMealplanPage implements AfterContentChecked {
+export class OwnMealplanPage implements OnInit {
   swiperModules = [IonicSlides];
   updating = false;
   history = {};
 
-  constructor(private storageService: StorageService) {
+  constructor(private storageService: StorageService, private eventAggregator: EventAggregatorService, private router: Router) {
     register();
   }
 
-  async ngAfterContentChecked() {
+  async ngOnInit(): Promise<void> {
+    if (!this.eventAggregator.appStarted.getValue()) {
+      this.router.navigate(['/'], { skipLocationChange: true });
+    }
+    await this.waitForStart().then(async () => {
+      await this.storageService.getHistory().then((history) => {
+        this.history = history;
+      });
+      console.log(this.history);
+    });
+  }
+
+  async waitForStart() {
+    while (!this.eventAggregator.appStarted.getValue()) {
+      await new Promise((resolve) => setTimeout(resolve, 300));
+    }
+  }
+
+  /*async ngAfterContentChecked() {
     if (!this.updating) {
       this.updating = true;
       await this.storageService.getHistory().then((history) => {
@@ -34,7 +54,7 @@ export class OwnMealplanPage implements AfterContentChecked {
       });
       console.log(this.history);
     }
-  }
+  }*/
 
   test(swiper: Swiper) {
     console.log(swiper.swipeDirection);
