8ec2eef40486635e74137d72c3534efca746d451
Marius ULFERS <marius.ulfers@outlook.de>
2023-11-09 23:05:20 +0100

    fixed unit tests

--- src/app/setup2/setup2.page.spec.ts
+++ src/app/setup2/setup2.page.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { Setup2Page } from './setup2.page';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Router } from '@angular/router';
+import { EventAggregatorService } from '../services/event-aggregator.service';
 
 describe('Setup2Page', () => {
   let component: Setup2Page;
   let fixture: ComponentFixture<Setup2Page>;
+  let eventAggregatorService: EventAggregatorService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -13,6 +15,8 @@ describe('Setup2Page', () => {
       providers: [Storage],
     }).compileComponents();
 
+    eventAggregatorService = TestBed.inject(EventAggregatorService);
+    eventAggregatorService.appStarted.next(true);
     fixture = TestBed.createComponent(Setup2Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,11 +26,14 @@ describe('Setup2Page', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should navigate to main page if not navigated', () => {
+  it('should navigate to main page if app not started', async () => {
+    eventAggregatorService.appStarted.next(false);
     let router = TestBed.inject(Router);
     let navigateSpy = spyOn(router, 'navigate');
     router.navigated = false;
     component.ngOnInit();
+    eventAggregatorService.appStarted.next(true);
+    await fixture.whenStable();
     expect(navigateSpy).toHaveBeenCalledWith(['']);
   });
 });
